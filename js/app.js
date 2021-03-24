e3f569c1cbfb264dfc02ab2472eb52067c514304
jenkins96 <adrianjnkns@gmail.com>
2021-03-24 15:40:41 +0000

    Done

--- js/app.js
+++ js/app.js
@@ -4,7 +4,9 @@ let myProps = {
     action: null,
     digit: null,
     operaciones: document.querySelector("#operaciones"),
-    maxQuantitySymb: 0
+    maxQuantitySymb: 0,
+    maxQuantityDec: false,
+    result: false
 }
 
 
@@ -18,6 +20,85 @@ let myMethod = {
         }
 
     },
+    keyboard: function () {
+        document.addEventListener("keydown", myMethod.press)
+
+    },
+    press: function (key) {
+
+
+
+        if (key.keyCode == 48 || key.keyCode == 96) {
+            myProps.action = "number";
+            myProps.digit = 0;
+        }
+        if (key.keyCode == 49 || key.keyCode == 97) {
+            myProps.action = "number";
+            myProps.digit = 1;
+        }
+        if (key.keyCode == 50 || key.keyCode == 98) {
+            myProps.action = "number";
+            myProps.digit = 2;
+        }
+        if (key.keyCode == 51 || key.keyCode == 99) {
+            myProps.action = "number";
+            myProps.digit = 3;
+        }
+        if (key.keyCode == 52 || key.keyCode == 100) {
+            myProps.action = "number";
+            myProps.digit = 4;
+        }
+        if (key.keyCode == 53 || key.keyCode == 101) {
+            myProps.action = "number";
+            myProps.digit = 5;
+        }
+        if (key.keyCode == 54 || key.keyCode == 102) {
+            myProps.action = "number";
+            myProps.digit = 6;
+        }
+        if (key.keyCode == 55 || key.keyCode == 103) {
+            myProps.action = "number";
+            myProps.digit = 7;
+        }
+        if (key.keyCode == 56 || key.keyCode == 104) {
+            myProps.action = "number";
+            myProps.digit = 8;
+        }
+        if (key.keyCode == 57 || key.keyCode == 105) {
+            myProps.action = "number";
+            myProps.digit = 9;
+        }
+        if (key.keyCode == 187 || key.keyCode == 107) {
+            myProps.action = "symbol";
+            myProps.digit = "+";
+        }
+        if (key.keyCode == 189 || key.keyCode == 109) {
+            myProps.action = "symbol";
+            myProps.digit = "-";
+        }
+        if (key.keyCode == 88 || key.keyCode == 106) {
+            myProps.action = "symbol";
+            myProps.digit = "*";
+        }
+        if (key.keyCode == 111) {
+            myProps.action = "symbol";
+            myProps.digit = "/";
+        }
+        if (key.keyCode == 190 || key.keyCode == 110) {
+            myProps.action = "decimal";
+            myProps.digit = ".";
+        }
+        if (key.keyCode == 13) {
+            myProps.action = "equal";
+        }
+        if (key.keyCode == 8) {
+            myProps.action = "";
+            myMethod.borrarCalculadora();
+        }
+
+        myMethod.calculator(myProps.action, myProps.digit);
+    },
+
     keyPress: event => {
 
         myProps.action = event.target.getAttribute("class");
@@ -35,10 +116,16 @@ let myMethod = {
                     myProps.operaciones.innerHTML = digit;
 
                 } else {
-                    myProps.operaciones.innerHTML += digit;
+                    if (myProps.result) {
+                        myProps.result = false;
+                        myProps.operaciones.innerHTML = digit;
+
+                    } else {
+                        myProps.operaciones.innerHTML += digit;
+                    }
+
 
                 }
-
                 break;
             //SYMBOL
             case "symbol":
@@ -48,6 +135,8 @@ let myMethod = {
                         myProps.operaciones.innerHTML = 0
                     } else {
                         myProps.operaciones.innerHTML += digit;
+                        myProps.maxQuantityDec = false;
+                        myProps.result = false;
                     }
 
                 }
@@ -55,18 +144,30 @@ let myMethod = {
                 break;
 
             case "decimal":
-                console.log("decimal");
+                if (!myProps.maxQuantityDec && myProps.maxQuantitySymb != 1) {
+
+                    myProps.operaciones.innerHTML += digit;
+                    myProps.maxQuantityDec = true;
+                    myProps.result = false;
+                }
                 break;
 
             case "equal":
-                console.log("equal");
+                myProps.operaciones.innerHTML = eval(myProps.operaciones.innerHTML);
+                let expression = /./g;
+                if (!expression.test(myProps.operaciones.innerHTML)) {
+                    myProps.maxQuantityDec = true;
+                }
+                myProps.result = true;
                 break;
         }
     },
     borrarCalculadora: _ => {
+        myProps.result = false;
         myProps.operaciones.innerHTML = 0;
     }
 }
 
 
-myMethod.init()
+myMethod.init();
+myMethod.keyboard();
